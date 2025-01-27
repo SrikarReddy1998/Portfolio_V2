faa489d17aec991fb589e39f44fb83f3a0b27c7c
srikarreddy1998 <master.srikarreddy@gmail.com>
2025-01-27 06:39:50 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   root: "./", // Your project root containing index.html
   build: {
     outDir: "dist", // Output folder for build files
+    rollupOptions: {
+      input: {
+        main: "index.html", // Your main entry point
+        script: "script.js", // Add `script.js` as an entry point
+      },
+    },
   },
-};
+});
